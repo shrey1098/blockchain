4c5d6244e2a32f33cbb20a6998f92c115f9e26d7
Shrey Sharma <54493774+shrey1098@users.noreply.github.com>
2021-06-22 15:32:34 +0530

    Update main.py

--- main/main.py
+++ main/main.py
@@ -6,16 +6,14 @@ walletKey = myKey.verifying_key
 
 coin = blockChain()
 
-tx1 = Transaction(walletKey, 'public key goes here', 10)
+tx1 = Transaction(fromAddress=walletKey, toAddress='public key goes here', amount=10)
 tx1.signTransaction(myKey)
-
 coin.addTransaction(tx1)
 
 coin.minePendingTransactions(walletKey)
 
-tx2 = Transaction(walletKey, 'public key goes here', 100)
+tx2 = Transaction(fromAddress=walletKey, toAddress='public key goes here', amount=100)
 tx2.signTransaction(myKey)
-
 coin.addTransaction(tx2)
 
 coin.minePendingTransactions(walletKey)
